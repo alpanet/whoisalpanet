85d9e7a4b3496ae8a19aa56e65250448a736f11e
AlpaslanDOGAN <alpaslan.dogan@visobl.com>
2025-02-16 23:38:36 +0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import nextMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
 import rehypePrism from "@mapbox/rehype-prism";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/whoisalpanet",
-  assetPrefix: "/whoisalpanet",
-  reactStrictMode: true,
-  images: {
-    domains: ["images.unsplash.com", "res.cloudinary.com"],
-  },
-};
-
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
@@ -21,4 +11,27 @@ const withMDX = nextMDX({
   },
 });
 
-export default withMDX(nextConfig);
+const nextConfig: NextConfig = withMDX({
+  output: "export",
+  basePath: "/whoisalpanet",
+  assetPrefix: "/whoisalpanet",
+  reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com", "res.cloudinary.com"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: { presets: ["next/babel"] },
+        },
+        "@mdx-js/loader",
+      ],
+    });
+    return config;
+  },
+});
+
+export default nextConfig;
