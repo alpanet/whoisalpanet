040564950ff5c71fb8668c211d6c03dbba0719ba
AlpaslanDOGAN <alpaslan.dogan@visobl.com>
2025-02-17 18:21:17 +0300

    again

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,6 @@
 const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: isProduction ? "export" : "standalone",
-  basePath: isProduction ? '/whoisalpanet' : '',
-  images: {
-    unoptimized: true,
-    domains: ['alpanet.github.io'],
-  },
 };
 
 export default nextConfig;
