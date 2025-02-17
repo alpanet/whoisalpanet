31d3d8106fb1cb3a65a9078da99617a81d517534
AlpaslanDOGAN <alpaslan.dogan@visobl.com>
2025-02-17 18:18:42 +0300

    again

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,11 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  output: isProduction ? "export" : "standalone",
   basePath: isProduction ? '/whoisalpanet' : '',
   images: {
     unoptimized: true,
     domains: ['alpanet.github.io'],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*).(js|css|woff|woff2|jpg|jpeg|png|gif|svg|ico|webp)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
   },
 };
 
