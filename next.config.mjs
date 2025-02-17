c4b910bb6ac5e53f66964969cb2782c2e20c8d5a
AlpaslanDOGAN <alpaslan.dogan@visobl.com>
2025-02-17 18:06:06 +0300

    file changed.

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
-import nextMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
-import rehypePrism from "@mapbox/rehype-prism";
-
 const nextConfig = {
-  output: "export",
-  images: {
-    domains: ["images.unsplash.com", "res.cloudinary.com"],
-  },
-  experimental: {
-    mdxRs: true,
-  },
+  output:'export',
 };
-
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
-});
-
-export default withMDX(nextConfig);
+/* module.exports = nextConfig*/
+export default nextConfig;
