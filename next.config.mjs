a81455738ef6f28ead92d39f09157e801e082085
AlpaslanDOGAN <alpaslan.dogan@visobl.com>
2025-02-17 19:07:25 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  output: isProduction ? "export" : "standalone",
-  basePath: isProduction ? '/whoisalpanet' : '',
-  images: {
-    unoptimized: true,
-    domains: ['alpanet.github.io'],
-  },
+  basePath: "/whoisalpanet",
+  output: "export",
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
