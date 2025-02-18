4457b82f09b7e6c063f64eeef6a24b90142e0a6d
AlpaslanDOGAN <alpaslan.dogan@visobl.com>
2025-02-18 18:33:07 +0300

    content changed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,16 @@ export default function Home() {
       <span className="text-4xl">👋</span>
       <Heading className="font-black">Hello there! I&apos;m Alpaslan</Heading>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a full-stack developer that loves{" "}
-        <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
+        {`I'm a Frontend UI/UX developer, designer, and storyteller who thrives on building seamless digital experiences. My journey started with C# and WinForms, evolving into Node.js, React, and UI/UX design with Figma. Along the way, I explored Open Banking, mobile development with Flutter, and even video editing for promotional content.`}
       </Paragraph>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a senior software engineer with{" "}
-        <Highlight>7 years of experience</Highlight> building scalable web apps
-        that are performance optimized and good looking.
+        {`Whether it's crafting scalable web apps, designing intuitive interfaces, or developing AI-driven solutions like my crypto trading bot, Alpanet-AI, I’m always pushing the boundaries of what’s possible.`}
       </Paragraph>
       <Heading
         as="h2"
         className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4"
       >
-        What I&apos;ve been working on
+        My Worked Projects
       </Heading>
       <Products />
       <TechStack />
