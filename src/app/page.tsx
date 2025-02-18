23dfc498381789316175c99a1e4edabf70e35e34
AlpaslanDOGAN <alpaslan.dogan@visobl.com>
2025-02-19 01:27:45 +0300

    bugfix and improve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
 import { Container } from "@/components/Container";
 import { Heading } from "@/components/Heading";
 import { Highlight } from "@/components/Highlight";
+import { LogoStack } from "@/components/LogoStack";
 import { Paragraph } from "@/components/Paragraph";
 import { Products } from "@/components/Products";
 import { TechStack } from "@/components/TechStack";
 import Image from "next/image";
+import { Typewriter } from "@/components/ui/typewriter"
 
 export default function Home() {
   return (
     <Container>
       <span className="text-4xl">👋</span>
-      <Heading className="font-black">Hello there! I&apos;m Alpaslan</Heading>
+      <div className="whitespace-pre-wrap text-3xl">
+        <span>{"Hi there I'm "}</span>
+        <Typewriter
+          text={[
+            "Alpanet",
+            "Alpaslan",
+          ]}
+          speed={70}
+          className="text-gray-500"
+          waitTime={1500}
+          deleteSpeed={40}
+          cursorChar={"_"}
+        />
+      </div>
       <Paragraph className="max-w-xl mt-4">
         {`I'm a Frontend UI/UX developer, designer, and storyteller who thrives on building seamless digital experiences. My journey started with C# and WinForms, evolving into Node.js, React, and UI/UX design with Figma. Along the way, I explored Open Banking, mobile development with Flutter, and even video editing for promotional content.`}
       </Paragraph>
@@ -24,7 +39,7 @@ export default function Home() {
         My Worked Projects
       </Heading>
       <Products />
-      <TechStack />
+      <LogoStack />
     </Container>
   );
 }
