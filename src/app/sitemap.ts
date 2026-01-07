26cbbe2e1efb2eafab475b7599a0d291f8bc2f53
AlpaslanDOGAN <alpaslanndogan@hotmail.com>
2026-01-08 01:10:48 +0300

    pipeline hatası

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next";
 import { siteConfig } from "@/constants/metadata";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
     const routes = [
         "",
